070636805cfa8c33d542f15d7217648fdbf1c4d4
Fransjo Leihitu <coreelements@MacBook-Pro-van-Fransjo.local>
2018-01-23 17:16:54 +0100

    update cli_console

--- tests/cli_console.php
+++ tests/cli_console.php
@@ -85,38 +85,115 @@ do {
   $command = fgets(STDIN);
   $command = strtolower(trim(preg_replace('/\s+/', '', $command)));
 
+  if($command == null)  {
+    cls();
+  }
+
   switch($command) {
 
     default : {
-      fwrite(STDOUT, "$prefixConsole [ERROR] I don't know that command!\n\n");
+      fwrite(STDOUT, "$prefixConsole [ERROR] I don't know that option!\n\n");
       break;
     }
 
-    case "x" : {
-        $command  = "q";
-    }
-    case "q" : {
+    case -1 : {
       break;
     }
 
-    case "m" : {
+    case 0 : {
       listMenu();
       break;
     }
 
-    case "cls" : {
+    case 1 : {
+      getTicker($exchange,$market);
+      break;
+    }
+
+    case 2 : { }
+    case 3 : { }
+    case 4 : { }
+    case 5 : {
+      fwrite(STDOUT, "$prefixConsole not ready yet!\n\n");
+    }
+
+    case 6 : {
+      fwrite(STDOUT, "$prefixConsole current market : $_market\n");
+      fwrite(STDOUT, "$prefixConsole enter a new market : ");
+      $command1 = fgets(STDIN);
+      $command1 = strtolower(trim(preg_replace('/\s+/', '', $command1)));
+      if($command1 != "") {
+        $command1 = strtoupper($command1);
+        fwrite(STDOUT, "$prefixConsole new market : $command1\n");
+        $_market  = $command1;
+        $market = $exchange->getMarketPair($_market , $_currency);
+        $prefixConsole  = "[" . $_exchange . " " . $exchange->getVersion() . " " . $market  . "]";
+        getTicker($exchange,$market);
+      }
+      break;
+    }
+
+    case 7 : {
+      fwrite(STDOUT, "$prefixConsole current currency : $_currency\n");
+      fwrite(STDOUT, "$prefixConsole enter a new currency : ");
+      $command1 = fgets(STDIN);
+      $command1 = strtolower(trim(preg_replace('/\s+/', '', $command1)));
+      if($command1 != "") {
+        $command1 = strtoupper($command1);
+        fwrite(STDOUT, "$prefixConsole new currency : $command1\n");
+        $_currency  = $command1;
+        $market = $exchange->getMarketPair($_market , $_currency);
+        $prefixConsole  = "[" . $_exchange . " " . $exchange->getVersion() . " " . $market  . "]";
+        getTicker($exchange,$market);
+      }
+      break;
+    }
+
+    case 10 : {
+      fwrite(STDOUT, "$prefixConsole current exchange : $_exchange\n");
+      fwrite(STDOUT, "$prefixConsole Active exchanges\n");
+      $counter  = 1;
+      $exchanges  = array();
+      foreach($config as $key=>$value) {
+        if(isSet($exchangesInstances[$key])) {
+          fwrite(STDOUT, "[$counter] $key\n");
+          $exchanges[$counter . "_exchange"]  = $key;
+          $counter++;
+        }
+      }
+      fwrite(STDOUT, "Select new exchange : ");
+      $selectOrder  = fgets(STDIN);
+      $selectOrder  = strtolower(trim(preg_replace('/\s+/', '', $selectOrder)));
+
+      if($selectOrder != null) {
+        $index        = $selectOrder . "_exchange";
+        $keyExchange  = isSet($exchanges[$index]) ? $exchanges[$index] : null;
+        if($keyExchange != null) {
+
+          fwrite(STDOUT, "New exchange selected : $keyExchange\n\n");
+
+          $exchange   = $exchangesInstances[$keyExchange];
+          $_exchange  = $keyExchange;
+          $market = $exchange->getMarketPair($_market , $_currency);
+          $prefixConsole  = "[" . $_exchange . " " . $exchange->getVersion() . " " . $market  . "]";
+          getTicker($exchange,$market);
+        }
+      }
+      break;
+    }
+
+    case 100 : {
       cls();
       break;
     }
 
-    case "t" : {
-      getTicker($exchange,$market);
-      break;
-    }
+
   }
 
-} while ($command != "q");
+} while ($command != -1);
 
+fwrite(STDOUT, "-- Game over --\n\n");
+die();
 
 function getTicker($exchange,$market) {
   fwrite(STDOUT, "** Fetching ticket information for $market\n");
@@ -138,15 +215,18 @@ function getTicker($exchange,$market) {
 }
 
 function listMenu() {
-  fwrite(STDOUT, "** List of command(s) :\n");
-  fwrite(STDOUT, "[q] quit\n");
-  fwrite(STDOUT, "[m] menu\n");
-  fwrite(STDOUT, "[t] get ticker information\n");
-  fwrite(STDOUT, "[s] sell units\n");
-  fwrite(STDOUT, "[b] buy units\n");
-  fwrite(STDOUT, "[o] get open orders\n");
-  fwrite(STDOUT, "[c] cancel orders\n");
-  fwrite(STDOUT, "[cls] clear screen\n");
+  fwrite(STDOUT, "** Menu :\n");
+  fwrite(STDOUT, "[-1] quit\n");
+  fwrite(STDOUT, "[0] menu\n");
+  fwrite(STDOUT, "[1] get ticker information\n");
+  fwrite(STDOUT, "[2] sell units\n");
+  fwrite(STDOUT, "[3] buy units\n");
+  fwrite(STDOUT, "[4] get open orders\n");
+  fwrite(STDOUT, "[5] cancel orders\n");
+  fwrite(STDOUT, "[6] change market\n");
+  fwrite(STDOUT, "[7] change currency\n");
+  fwrite(STDOUT, "[10] change exchange\n");
+  fwrite(STDOUT, "[100] clear screen\n");
   fwrite(STDOUT, "\n");
 }
 ?>
