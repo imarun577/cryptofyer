4dfbe0ab1a49e69509259c00ab76c21c311bd921
Fransjo Leihitu <coreelements@MacBook-Pro-van-Fransjo.local>
2018-01-25 01:02:09 +0100

    update exchange template

--- _exchange/exchange_api.class.php
+++ _exchange/exchange_api.class.php
@@ -33,6 +33,56 @@
         parent::setCurrencyAlias($this->currencyAlias);
     }
 
+    private function send($method = null , $args = array() , $secure = true , $type="GET") {
+      if(empty($method)) return array("status" => false , "error" => "method was not defined!");
+
+      if(isSet($args["market"])) unset($args["market"]);
+
+      $fields = null;
+      if(!empty($args)) {
+        ksort($args);
+        $fields     = !empty($args) ? http_build_query($args, '', '&') : "";
+      }
+      $sign       = strtoupper(hash_hmac('sha256', $fields, $this->apiSecret));
+
+      $args["nonce"] = time();
+
+      $uri  = $this->getBaseUrl() . $method;
+      $uri  .= (!empty($fields) && $type == "GET") ? "?" . $fields : "";
+
+      $ch   = curl_init($uri);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+      if($secure) {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+          'API-key: '.$this->apiKey,
+          'Sign: '.$sign
+        ));
+      }
+
+      if($type == "POST") {
+        curl_setopt($ch, CURLOPT_POST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+      }
+
+      $execResult = curl_exec($ch);
+
+      // check if there's a curl error
+      if(curl_error($ch)) return $this->getErrorReturn(curl_error($ch));
+
+      // try to convert json repsonse to assoc array
+      if($obj = json_decode($execResult , true)) {
+        if($obj !== null) {
+          return $this->getReturn(true,"",$obj);
+        } else {
+          return $this->getErrorReturn("server error");
+        }
+      } else {
+        return $this->getErrorReturn($execResult);
+      }
+      return $this->getErrorReturn("unknown error");
+    }
+
 
     // get ticket information
     public function getTicker($args  = null) {
